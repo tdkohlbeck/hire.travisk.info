7b6a0a4ad6c08a6b6c92f32281e86bd3fc391a94
Travis Kohlbeck <me+git@travisk.info>
2019-08-02 23:12:12 -0400

    adds text, list and icon, changes readme bullet glyphs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+import gear_icon from './gear.svg'
+
+const main_pitch = `I am a novice web developer and designer looking for an entry-level position with opportunity to learn and grow. I work with standard web technologies [1](HTML + CSS + JS), modern tools and libraries[2] and unix-based servers[3]`
+const engineering_blurb = 'I work with code, but my degree\'s in engineering, so I know how to solve problems and coordinate with teams.'
 
 function App() {
   return (
     <article>
       <header>Hi, I'm Travis</header>
-      <main>
-
-      </main>
+      <main>{main_pitch}</main>
       <section id='big'>
-
+        <ul>
+          <li>
+            <img src={gear_icon} alt='mechanical gear icon' />
+            {engineering_blurb}
+          </li>
+        </ul>
       </section>
       <section id='medium'>
 
