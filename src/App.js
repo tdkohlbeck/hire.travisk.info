cb5d4bf087772589bcba7e4df3ed2d64e3797e9f
tdkohlbeck <me+git@travisk.info>
2019-08-03 16:54:51 -0400

    big list item layout modifications

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import './App.css'
-import gear_icon from './gear.svg'
+import gear_icon from './white-gear.svg'
 
-const main_pitch = `I am a novice web developer and designer looking for an entry-level position with opportunity to learn and grow. I work with standard web technologies [1](HTML + CSS + JS), modern tools and libraries[2] and unix-based servers[3]`
+const main_pitch = `I am a novice web developer and designer looking for an entry-level position with opportunity to learn and grow. I work with standard web technologies [1](HTML + CSS + JS), modern tools and libraries[2] and unix-based command-lines[3]`
 const engineering_blurb = 'I work with code, but my degree\'s in engineering, so I know how to solve problems and coordinate with teams.'
 
 function App() {
   return (
     <article>
-      <header>Hi, I'm Travis</header>
+      <header>Hi, I'm Travis.</header>
       <main>{main_pitch}</main>
       <section id='big'>
         <ul>
           <li>
-            <img src={gear_icon} alt='mechanical gear icon' />
-            {engineering_blurb}
+            <img src={gear_icon} id='gear' alt='mechanical gear icon' />
+            <span>{engineering_blurb}</span>
           </li>
         </ul>
       </section>
